8e150a0079c7586df997f3fd9f3c99a168179cdb
olvrcrls <dev.olivercarlos@gmail.com>
2022-11-27 10:48:03 +0800

    feature(blog-config): Configuration to compute estimated reading time for an article.

---
+++ config/blog.php
@@ -0,0 +1,36 @@
+<?php
+
+return  [
+    /*
+    |--------------------------------------------------------------------------
+    | Blog Defaults
+    |--------------------------------------------------------------------------
+    |
+    | These rule sets control the default blog configuration such as the
+    | total words per minute to compute the reading length of an article.
+    |
+    */
+
+    /*
+    |--------------------------------------------------------------------------
+    | Words per Minute
+    |--------------------------------------------------------------------------
+    |
+    | This option controls how much estimated time will be spent on
+    | reading the blog article.
+    |
+    */
+    'words_per_minute' => 200,
+
+    /*
+    |--------------------------------------------------------------------------
+    | After Label Display
+    |--------------------------------------------------------------------------
+    |
+    | This option controls the label text to display after
+    | the computation.
+    |
+    */
+    'after_label_display' => 'minutes',
+
+];
